42c2492e65014f39010490db1e9faa25ec1027cf
Jesse Manninen <jesse.manninen@gmail.com>
2023-09-18 20:53:45 +0300

    change homepage margin

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,40 +2,39 @@ import { Box, Container, Divider, Link, Typography } from "@mui/material"
 
 export default function Home() {
   return (
-    <Container sx={{ marginY: 5 }} maxWidth="md">
-      <Typography variant="h3" component="h2" marginTop={5} marginBottom={3}>
-        Countries app is a simple React application made in Business College
-        Helsinki lessons.
-      </Typography>
-
-      <Divider />
-      <Typography
-        variant="subtitle1"
-        component="h3"
-        marginTop={4}
-        marginBottom={1}
-      >
-        App uses:
-      </Typography>
-
-      <Box>
-        <Link
-          href="https://restcountries.com/"
-          rel="noreferrer"
-          target="_blank"
+    <Container maxWidth="md">
+      <Box my="25px" px="25px">
+        <Typography variant="h3" component="h2" marginBottom={3}>
+          Countries app is a simple React application made in Business College
+          Helsinki lessons.
+        </Typography>
+        <Divider />
+        <Typography
+          variant="subtitle1"
+          component="h3"
+          marginTop={3}
+          marginBottom={1}
         >
-          https://restcountries.com/
-        </Link>
-      </Box>
-
-      <Box>
-        <Link
-          href="https://openweathermap.org/"
-          rel="noreferrer"
-          target="_blank"
-        >
-          https://openweathermap.org/
-        </Link>
+          App uses:
+        </Typography>
+        <Box>
+          <Link
+            href="https://restcountries.com/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            https://restcountries.com/
+          </Link>
+        </Box>
+        <Box>
+          <Link
+            href="https://openweathermap.org/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            https://openweathermap.org/
+          </Link>
+        </Box>
       </Box>
     </Container>
   )
