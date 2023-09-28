b62fd5aaa05bc49d745e109c710b74d68db31755
Jesse Manninen <jesse.manninen@gmail.com>
2023-09-28 21:15:01 +0300

    changed app folder to redyx folder

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { RouterProvider } from "react-router-dom"
-import { store } from "./app/store"
+import { store } from "./redux/store"
 import ThemeContextProvider from "./context/ThemeContext"
 import { router } from "./router/router"
 
